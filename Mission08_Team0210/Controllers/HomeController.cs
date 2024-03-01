264e9b2fbb4fa8c1d779bd48bbbeae19e9600474
DJergensen13 <157402971+DJergensen13@users.noreply.github.com>
2024-02-29 17:34:09 -0700

    Quadrants view

--- Mission08_Team0210/Controllers/HomeController.cs
+++ Mission08_Team0210/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Mission08_Team0210.Models;
 using System.Diagnostics;
 
@@ -6,16 +7,29 @@ namespace Mission08_Team0210.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private ToDoListContext _context;
+        public HomeController(ToDoListContext temp) // constructor
         {
-            _logger = logger;
+            _context = temp;
         }
+        // private readonly ILogger<HomeController> _logger;
+
+        // public HomeController(ILogger<HomeController> logger)
+        // {
+        //     _logger = logger;
+        // }
 
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Quadrants()
+        {
+            var quadrants = _context.Tasks
+                .OrderBy(x => x.TaskId).ToList();
+
+            return View(quadrants);
         }
     }
 }
